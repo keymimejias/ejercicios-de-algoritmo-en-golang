4c007ca789591af8af639456d08e8330136fecde
keymimejias <keymikabel@gmail.com>
2022-12-13 15:47:17 -0400

    Modificandoelcodigo

--- Proyecto-Palidrome/string-palindrome.go
+++ Proyecto-Palidrome/string-palindrome.go
@@ -4,17 +4,20 @@ import "fmt"
 
 func main() {
 
-	pali := []string{"o", "t", "t", "o"}
+	pali := "otto"
 
-	for i := 0; i < len(pali); i++ {
-		if i < 4 {
+	for _, i := range pali {
+
+		//fmt.Printf("%c\n", i)
+
+		if i < 3 {
+
 			if pali[i] == pali[i+3] {
 
 				fmt.Println(true)
 
 				fmt.Println("La palabra", pali, "si es palindrome")
 				//break
-
 			} else {
 				fmt.Println(false)
 
@@ -23,5 +26,4 @@ func main() {
 			}
 		}
 	}
-
 }
